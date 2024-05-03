b07ad2086d5c7aa5ea3e467112bf6a5cd08118ea
Frank Pitkat <fpitkat@gmail.com>
2024-05-03 12:31:12 -0400

    chore: Update manifest.json and Product.jsx, add ProductScreen component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,11 +6,11 @@ import Rating from './Rating';
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product.id}`}>
+      <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </a>
       <Card.Body>
-        <Link to={`/product/${product.id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div' className='product-title'>
             <strong>{product.name}</strong>
           </Card.Title>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import '../src/assets/styles/index.css';
 import App from './App';
 import './assets/styles/bootstrap.custom.css';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 /**
  * The router object for handling navigation and rendering of routes.
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
